02cc607ce51c0bd16ffce9285de5aa17de79f7d6
Rajat Venkatesh <rvenkatesh@qubole.com>
2015-11-05 11:28:38 +0530

    Reorganize reporters to deduplicate code. Rename modules correctly
    Code to submit data points was copy-pasted in 3 files. Deduplicated
    by adding a base class. Code to create data points is still unique
    to each class.

--- lib/metriks_addons/cloudwatch_reporter.rb
+++ lib/metriks_addons/cloudwatch_reporter.rb
@@ -2,12 +2,14 @@ require 'metriks/time_tracker'
 require 'logger'
 require 'aws'
 require 'time'
+require 'metriks_addons/base_reporter'
 
-module Metriks
-  class CloudWatchReporter
+module MetriksAddons
+  class CloudWatchReporter < BaseReporter
     attr_accessor :prefix, :source, :cw, :tags, :logger
 
     def initialize(access_key, secret_key, namespace, tags, options = {})
+      super(options)
       @cw = AWS::CloudWatch.new(:access_key_id => access_key, :secret_access_key => secret_key)
       @namespace = namespace
       @dimensions = get_dimensions(tags)
@@ -15,70 +17,7 @@ module Metriks
       @prefix = options[:prefix]
       @source = options[:source]
 
-      @logger       = options[:logger] || nil
       @batch_size   = options[:batch_size] || 50
-      @registry     = options[:registry] || Metriks::Registry.default
-      @interval     = options[:interval] || 60
-      @time_tracker = Metriks::TimeTracker.new(@interval)
-      @on_error     = options[:on_error] || proc { |ex| }
-
-      if options[:percentiles]
-        @percentiles = options[:percentiles]
-      else
-        @percentiles = [ 0.95, 0.99]
-      end
-
-      @mutex = Mutex.new
-      @running = false
-    end
-
-    def log(level, msg)
-      if !@logger.nil?
-        @logger.send level, msg
-      end
-    end
-
-    def start
-      if @thread && @thread.alive?
-        return
-      end
-
-      @running = true
-      @thread = Thread.new do
-        while @running
-          @time_tracker.sleep
-
-          Thread.new do
-            flush
-          end
-        end
-      end
-    end
-
-    def stop
-      @running = false
-
-      if @thread
-        @thread.join
-        @thread = nil
-      end
-    end
-
-    def restart
-      stop
-      start
-    end
-
-    def flush
-      begin
-        @mutex.synchronize do
-          log "debug", "Flushing metrics"
-          submit get_datapoints
-        end
-      rescue Exception => ex
-        log "error",ex.message
-        @on_error[ex] rescue nil
-      end
     end
 
     def submit(datapoints)
