5cfae3b458d598558c649de433dee8d31b3cea9d
Rajat Venkatesh <rvenkatesh@qubole.com>
2015-11-05 15:55:39 +0530

    Fix wrong directory name introduced while renaming directories

--- lib/metriks-addons.rb
+++ lib/metriks-addons.rb
@@ -1,7 +1,7 @@
-require 'metriks_addons/opentsdb_reporter'
-require 'metriks_addons/signalfx_reporter'
-require 'metriks_addons/cloudwatch_reporter'
+require 'metriks-addons/opentsdb_reporter'
+require 'metriks-addons/signalfx_reporter'
+require 'metriks-addons/cloudwatch_reporter'
 
 module MetriksAddons
-  VERSION = '3.0.0'
+  VERSION = '3.0.1'
 end
